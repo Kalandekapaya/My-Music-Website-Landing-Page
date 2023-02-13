1d9a4300dadac024753fd0f979345586e310eb9e
Kalande_Kapaya <kjkreed@gmail.com>
2023-02-13 10:25:09 +0200

    initFunctional startComponents

---
+++ studio1964/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        I am here to help you
+
+      </header>
+    </div>
+  );
+}
+
+export default App;
